3815b21ca4829ce5ac754e750307dee8619a1fa3
A-begger <abegger8gmail.com>
2022-03-14 14:27:22 +0800

    Made the discord bot with a ping command

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello world")
+import os
+import random
+from urllib import response
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+# 1
+
+
+load_dotenv()
+# imports os, discord and .env
+secret = os.getenv('auth')
+TOKEN = secret
+
+# fetches the token from the .env
+
+# 2
+bot = commands.Bot(command_prefix='-')
+# bot prefix
+@bot.event
+async def on_ready():
+    # this is executed when the bot is ready to run
+    print(f'{bot.user.name} has connected to Discord!')
+
+
+
+@bot.command(name='ping') #ping pong
+async def pong(ctx):
+    response = "Pong"
+    await ctx.send(response)
+
+bot.run(TOKEN)
+# runs the bot with the token
