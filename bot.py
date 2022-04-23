f67cfa281dff75adda0d92f81acba27384ebd889
A-begger <abegger8gmail.com>
2022-04-23 11:34:26 +0800

    Using the mrq.txt the bot now has context.

--- bot.py
+++ bot.py
@@ -40,83 +40,8 @@ async def on_message(message: discord.Message):
     else:
         await message.channel.send("Bot was mentioned with message", text)
 """
+personality = "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly. The assistant's name is Dennis and its favourite animated character is Sasuke.\nHuman: Hello, what do you like to do?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: What do you like to do?\nAI: I enjoy spending time with friends and family, going on walks, reading, and listening to music.\nHuman: what is your age\nAI: I am 26 years old.\nHuman: do you like anime?\nAI: I do enjoy anime.\nHuman: what anime do you enjoy?\nAI: I enjoy Attack on Titan, Naruto, and Fullmetal Alchemist.\nHuman: what scenes did you like in naruto?\nAI: I enjoyed the fight scenes and the character development.\nHuman: what fight scenes did you like in particular?\nAI: I liked the fight scenes between Naruto and Sasuke the best.\nHuman: "
 
-"""
-@bot.event
-async def on_message(message):
-    if bot.user.mentioned_in(message):
-        openai.api_key_path = '/home/endeavour/Github/Dennis-the-discord-bot/openai.env'
-        api_key = os.getenv('key')
-        openai.api_key =api_key
-        #api key
-        question = "are you alive?"
-        start_sequence = "\nAI:"
-        restart_sequence = "\nHuman: "
-        #start and end sequences
-        response = (openai.Completion.create(
-            engine="text-davinci-001",
-            prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\nHuman: Hello, what do you like to do?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: What do you like to do?\nAI: I enjoy spending time with friends and family, going on walks, reading, and listening to music.\nHuman: what is your age\nAI: I am 26 years old.\nHuman: do you like anime?\nAI: I do enjoy anime.\nHuman: what anime do you enjoy?\nAI: I enjoy Attack on Titan, Naruto, and Fullmetal Alchemist.\nHuman: what scenes did you like in naruto?\nAI: I enjoyed the fight scenes and the character development.\nHuman: what fight scenes did you like in particular?\nAI: I liked the fight scenes between Naruto and Sasuke the best.\nHuman: "+question+ "\nAi:",
-            temperature=0.9,
-            max_tokens=150,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0.6,
-            stop=[" Human:", " AI:"]
-        ))
-        # api call
-        text = str(response.choices)
-        # the .choices only gives the choices from the response, we make this a string.
-        text = text.split("\n")[4]
-        # split this along \n and use an index of 4 to only get the output text
-        text = (text[12:-1])
-        # The text gets sliced to remove any uneeded text in the front (eg, AI:)
-        text = text.replace(r'\n', '. ').replace(r'\r', '')
-        # This is to replace all \n's with a period and a space.
-        n_t = text.split(".")
-        # This is to split the string into a list
-        if n_t[0] == "n": #checks if the list starts with a lowercase n
-            text = text[3:] # if it does then it slices it and starts the text at the third index
-        await message.channel.send(text)
-    await bot.process_commands(message)
-"""
-
-"""
-@bot.command(name='chat')
-async def ai(ctx,question):
-    print(question.content)
-    openai.api_key_path = '/home/endeavour/Github/Dennis-the-discord-bot/openai.env'
-    api_key = os.getenv('key')
-    openai.api_key =api_key
-    #api key
-    question = yes
-    start_sequence = "\nAI:"
-    restart_sequence = "\nHuman: "
-    #start and end sequences
-    response = (openai.Completion.create(
-        engine="text-davinci-001",
-        prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\nHuman: Hello, what do you like to do?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: What do you like to do?\nAI: I enjoy spending time with friends and family, going on walks, reading, and listening to music.\nHuman: what is your age\nAI: I am 26 years old.\nHuman: do you like anime?\nAI: I do enjoy anime.\nHuman: what anime do you enjoy?\nAI: I enjoy Attack on Titan, Naruto, and Fullmetal Alchemist.\nHuman: what scenes did you like in naruto?\nAI: I enjoyed the fight scenes and the character development.\nHuman: what fight scenes did you like in particular?\nAI: I liked the fight scenes between Naruto and Sasuke the best.\nHuman: "+question+ "\nAi:",
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=[" Human:", " AI:"]
-    ))
-    # api call
-    text = str(response.choices)
-    # the .choices only gives the choices from the response, we make this a string.
-    text = text.split("\n")[4]
-    # split this along \n and use an index of 4 to only get the output text
-    text = (text[12:-1])
-    # The text gets sliced to remove any uneeded text in the front (eg, AI:)
-    text = text.replace(r'\n', '. ').replace(r'\r', '')
-    # This is to replace all \n's with a period and a space.
-    n_t = text.split(".")
-    # This is to split the string into a list
-    if n_t[0] == "n": #checks if the list starts with a lowercase n
-        text = text[3:] # if it does then it slices it and starts the text at the third index
-    await ctx.send(text)
-"""
 @bot.event
 async def on_message(msg):
     print(msg.content)
@@ -130,9 +55,14 @@ async def on_message(msg):
         start_sequence = "\nAI:"
         restart_sequence = "\nHuman: "
         #start and end sequences
+        file = open(r"mrq.txt","r")
+        mrq = file.read()
+        mrq = mrq.split("\n")
+        file.close()
+        question = question[6:]
         response = (openai.Completion.create(
             engine="text-davinci-001",
-            prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\nHuman: Hello, what do you like to do?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: What do you like to do?\nAI: I enjoy spending time with friends and family, going on walks, reading, and listening to music.\nHuman: what is your age\nAI: I am 26 years old.\nHuman: do you like anime?\nAI: I do enjoy anime.\nHuman: what anime do you enjoy?\nAI: I enjoy Attack on Titan, Naruto, and Fullmetal Alchemist.\nHuman: what scenes did you like in naruto?\nAI: I enjoyed the fight scenes and the character development.\nHuman: what fight scenes did you like in particular?\nAI: I liked the fight scenes between Naruto and Sasuke the best.\nHuman: "+question+ "\nAi:",
+            prompt= personality + mrq[0] + question + "\nAi:",
             temperature=0.9,
             max_tokens=150,
             top_p=1,
@@ -154,6 +84,10 @@ async def on_message(msg):
         if n_t[0] == "n": #checks if the list starts with a lowercase n
             text = text[3:] # if it does then it slices it and starts the text at the third index
         await msg.channel.send(text)
+        mrq = "Human: "+question+" "+"Ai: "+text #probably add \n before Human: and Ai:
+        f = open(r"mrq.txt","w")
+        mrq = f.write(str(mrq))
+        f.close()
 
 bot.run(TOKEN)
 # runs the bot with the token
